7ae6bb0add54099d9f2b80a064d121b491105b08
shashwat047 <101187721+shashwat047@users.noreply.github.com>
2022-04-09 20:52:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,142 @@
+var bow , arrow,  background;
+var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
+var SCORE = 0
+function preload(){
+  
+  backgroundImage = loadImage("background0.png");
+  arrowImage = loadImage("arrow0.png");
+  bowImage = loadImage("bow0.png");
+  red_balloonImage = loadImage("red_balloon0.png");
+  blue_balloonImage= loadImage("blue_balloon0.png");
+  green_balloonImage = loadImage("green_balloon0.png");
+  pink_balloonImage = loadImage("pink_balloon0.png");
+}
+
+
+
+function setup() {
+  createCanvas(400, 400);
+  
+  //creating background
+  scene = createSprite(0,0,400,400);
+  scene.addImage(backgroundImage);
+  scene.scale = 2
+  
+  // creating bow to shoot arrow
+  bow = createSprite(380,220,20,50);
+  bow.addImage(bowImage); 
+  bow.scale = 1;
+  
+score = 0
+
+}
+
+function draw() {
+ background(0);
+
+ 
+ 
+
+ bow.depth = bow.depth +9;
+ 
+
+  // moving ground
+    scene.velocityX = -3 
+
+    if (scene.x < 0){
+      scene.x = scene.width/2;
+    }
+  
+
+    
+  //moving bow
+  bow.y = World.mouseY
+  
+   // release arrow when space key is pressed
+  if (keyDown("space")) {
+    createArrow();
+    
+  }
+  
+  //creating continous balloons
+  var select_balloon = Math.round(random(1,4));
+  
+  if (World.frameCount % 20 == 0) {
+    if (select_balloon == 1) {
+      redBalloon();
+    }
+    if (select_balloon ==2) {
+        blueBalloon();
+      }
+    if (select_balloon ==3){
+      greenBalloon();
+    }
+    if (select_balloon == 4){
+    pinkBalloon();
+    }
+    if (select_balloon== 1){
+      createArrow();
+    }
+    if (select_balloon==3){
+      createArrow();
+    }
+  }
+
+  
+  drawSprites();
+
+  
+  textSize (20);
+  text("score:"+score , 270,30);
+  score.depth = bow.depth+2
+  
+}
+
+
+// Creating  arrows for bow
+ function createArrow() {
+  var arrow= createSprite(400, 100, 60, 10);
+  arrow.addImage(arrowImage);
+  arrow.y=bow.y;
+  arrow.velocityX = -4;
+  arrow.lifetime = 150;
+  arrow.scale = 0.3;
+  arrow.depth = arrow.depth + 4
+}
+
+
+function redBalloon() {
+  var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  red.addImage(red_balloonImage);
+  red.velocityX = 3;
+  red.lifetime = 150;
+  red.scale = 0.1;
+
+}
+
+function blueBalloon() {
+  //write code for spwaning blue balloons
+  var blue = createSprite(0,Math.round(random(20,370)),10,10);
+  blue.addImage(blue_balloonImage);
+  blue.velocityX = 3;
+  blue.lifeTime = 150;
+  blue.scale = 0.1
+}
+
+function greenBalloon() {
+  //write code for spwaning green balloons
+  var green  = createSprite(0,Math.round(random(20,370)),10,10);
+  green.addImage (green_balloonImage);
+  green.velocityX = 3;
+  green.lifetime = 150;
+  green.scale = 0.1;
+}
+
+function pinkBalloon() {
+  //write code for spwaning pink balloons
+  var pink = createSprite(0,Math.round(random(20,370)),10,10);
+  pink.addImage (pink_balloonImage);
+  pink.velocityX  = 3;
+  pink.lifeTime = 150;
+  pink.scale = 1.3;
+}
